fb338f0391a7191647b3f58027d80e55981edf23
chel313 <handarovd@gmail.com>
2026-05-24 11:02:04 +0800

    Update db_script.py 1

--- db_script.py
+++ db_script.py
@@ -1,22 +1,73 @@
-# import sqlite3
+import sqlite3
+from random import randint
+db_name = 'test.sqlite'
+conn = None
+cursor = None
 
-# conn = None
-# cursor = None
-# def open_db():
-#     global conn, cursor
-#     conn = sqlite3.connect(db_name)
-#     cursor = conn.cursor()
+def open_db():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
 
-# def close():
-#     cursor.close()
-#     conn.close()
+def close():
+    cursor.close()
+    conn.close()
 
-# def do(query):
-#     cursor.execute(query)
-#     conn.commit()
+def do(query):
+    cursor.execute(query)
+    conn.commit()
 
-# def create():
-#     do('''CREATE TABLE IF NOT EXISTS user(id PRIMARY KEY,name VARCHAR,)''') 
-#     do('''CREATE TABLE IF NOT EXISTS questions(id PRIMARY KEY,question TEXT,true_answer TEXT,wrong_1 TEXT,
-#     wrong_2 TEXT''')
-#     do('''CREATE TABLE IF NOT EXISTS regions(id PRIMARY KEY,region VARCHAR)''')
+def clear_db():
+    ''' удаляет все таблицы '''
+    open_db()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close()
+
+    
+def create():
+    open_db()
+    cursor.execute('''PRAGMA foreign_key=on''')
+
+    do('''
+    CREATE TABLE IF NOT EXISTS quiz (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR)
+    ''')
+    
+    do('''
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY,
+        username NAME)
+    ''')
+
+    do('''
+    CREATE TABLE IF NOT EXISTS regions(
+        id INTEGER PRIMARY KEY,
+        region TEXT)
+    ''')
+
+    do('''
+    CREATE TABLE IF NOT EXISTS question (
+        id INTEGER PRIMARY KEY,
+        question TEXT,
+        answer TEXT,
+        wrong1 TEXT,
+        wrong2 TEXT
+    )
+    ''')
+    
+    close()
+
+def rendom_choice():
+    open_db()
+    regions = [
+
+    ]
+    cursor.execute('''PRAGMA foreing key=on''')
+    cursor.executemant("INSERT INTO regions(id,region)"(regions))
+    close()
